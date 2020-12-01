e27d79b0a6f25081ee7563d62d64ee707ab1d173
Juliane Reinhardt <jreinhardt@lbl.gov>
2020-12-01 09:54:04 -0800

    copy some basics from XPCS converter

--- nx_creator.py
+++ nx_creator.py
@@ -1,3 +1,7 @@
+import datetime
+import logging
+import os
+
 # TODO
 # [ ] create header
 # [ ] create entry group
@@ -8,3 +12,38 @@
 #       [ ] create positioner groups
 #   [ ] create data group
 # [ ] create process groups
+
+logger = logging.getLogger(__name__)
+
+def write_file_header(h5parent):
+    """optional header metadata"""
+    timestamp = datetime.datetime.now().isoformat(sep=" ", timespec="seconds")
+    logger.debug('timestamp: %s', str(timestamp))
+
+    # give the HDF5 root some more attributes
+    h5parent.attrs['file_name'] = h5parent.filename
+    h5parent.attrs['file_time'] = timestamp
+    h5parent.attrs['instrument'] = 'APS XPCS at 8-ID-I'
+    h5parent.attrs['creator'] = __file__             # TODO: better choice?
+    h5parent.attrs['HDF5_Version'] = h5py.version.hdf5_version
+    h5parent.attrs['h5py_version'] = h5py.version.version
+
+
+def create_entry_group(h5parent, md):
+    """
+    all information about the measurement
+
+    see: https://manual.nexusformat.org/classes/base_classes/NXentry.html
+    """
+    nxentry = h5parent.create_group('entry')
+    nxentry.attrs['NX_class'] = 'NXentry'
+
+    title = os.path.splitext(md["datafilename"])[0]
+    ds = nxentry.create_dataset('title', data=title)
+    ds.attrs["target"] = ds.name      # we'll re-use this
+    logger.debug("title: %s", title)
+
+    # NeXus structure: point to this group for default plot
+    h5parent.attrs['default'] = nxentry.name.split("/")[-1]
+
+    return nxentry
